5f69340b696e37307f622d590f8309a6866f624a
gerpi <gerpi.2666@gmail.com>
2023-05-21 19:20:00 -0600

    pp[

---
+++ appHardTech/Models/pp.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace appHardTech.Models
+{
+    public class PP
+    {
+      public int Id {get,set};
+      
+    }
+}
