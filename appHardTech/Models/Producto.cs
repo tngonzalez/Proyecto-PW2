f5b0c27d34a158e156e945aa4f459bfcedffc62e
Tylinn <tgonzalezb@est.utn.ac.cr>
2023-05-24 00:40:16 -0600

    iu

---
+++ appHardTech/Models/Producto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace appHardTech.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
